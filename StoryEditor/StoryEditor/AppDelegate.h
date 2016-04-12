d04a06891e59ba7ccc8738c1e418ec32dfe20269
loveSherlock <fanxz@me.com>
2016-04-12 11:12:43 -0400

    start

---
+++ StoryEditor/StoryEditor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryEditor
+//
+//  Created by FANXUEZHOU on 16/4/12.
+//  Copyright © 2016年 FANXUEZHOU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ StoryEditor/StoryEditor/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  StoryEditor
+//
+//  Created by FANXUEZHOU on 16/4/12.
+//  Copyright © 2016年 FANXUEZHOU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
